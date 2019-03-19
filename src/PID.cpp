5de3142c68edcf06720f7aaeee9663cc301d0bb4
Juan Rios <juandarr@gmail.com>
2019-03-18 21:58:39 -0500

    feat: complete pid initialization step.

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,8 +10,11 @@ PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
+   * Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 
 }
 
